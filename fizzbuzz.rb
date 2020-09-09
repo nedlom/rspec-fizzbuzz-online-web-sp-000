c23abb853de2311edf3cda2725f5a26c07944326
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-09 00:28:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,24 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def five(num)
+  num % 5 == 0
+end
+
+def three(num)
+  num % 3 == 0
+end
+
+def fizzbuzz(num)
+  if five(num) && three(num)
+    return 'FizzBuzz'
+  end
+  
+  if three(num)
+    return 'Fizz'
+  end
+  
+  if five(num)
+    return 'Buzz'
+  end
+end
